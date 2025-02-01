cf9886df05f27c412b50b3698b46f87fbd39b70f
Alondrag360 <alondrag4265@gmail.com>
2025-02-01 02:28:06 +0000

    Variables

---
+++ src/MixMatch.java
@@ -0,0 +1,28 @@
+public class MixMatch{
+    public static void main (String[] args){
+        String word = "Hello";
+        System.out.println(word + " String is used to store and manipulate text data.");
+
+        char a = 'A';
+        System.out.println(a + " Char is used to store a single character that could be a letter, digit, or symbol.");
+
+        short myNum = 5;
+        System.out.println(myNum + " Short can store whole numbers from -32,768 to 32,767.");
+
+        int num = 15;
+        System.out.println(num + " Integer can store values from -2,147,483,648 to 2,147,647.");
+
+        long b = 3L;
+        System.out.println(b + " Long is used to store numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807.");
+
+        float x = 10.54f;
+        System.out.println(x + " Float can represent decimal values up to 7 digits of precision.");
+
+        double y = 20.50;
+        System.out.println(y + " Double is used to store decimal numbers with high precision.");
+
+        boolean b1 = true;
+        System.out.println(b1 + " Boolean is a data type that can only hold two values: true or false.");
+
+    }
+}
